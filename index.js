83d38e3159d629a006bb3cccd1bb11f9a5a0adf2
rocky <rb@dustyfeet.com>
2018-10-23 19:44:38 -0400

    analyze calls compile first...
    This ensures we some (and the most up-to-date) JSON to send to mythril

--- index.js
+++ index.js
@@ -25,68 +25,83 @@ function getFormatter(style) {
 
 const Analyze = {
   run: function(options, done) {
+
     var Config = require("truffle-config");
     var config = Config.detect(options);
     const rootDir = config.working_directory;
     const buildDir = config.contracts_build_directory;
+    options.logger = options.logger || console;
 
-    // FIXME: use truffle library routine
-    const contractsDir = trufstuf.getContractsDir(rootDir);
-    const buildJson = trufstuf.guessTruffleBuildJson(buildDir);
 
-    // const expect = require("truffle-expect");
-    // FIXME: expect things
+    function analyzeWithBuildDir() {
+      // FIXME: use truffle library routine
+      const contractsDir = trufstuf.getContractsDir(rootDir);
+      let buildJson = trufstuf.guessTruffleBuildJson(buildDir);
 
-    var solidityFileBase;
-    let solidityFile;
-    let buildJsonPath;
+      // const expect = require("truffle-expect");
+      // FIXME: expect things
 
-    try {
-      if (options._.length === 0) {
-        solidityFileBase = path.basename(buildJson, '.json');
-      } else {
-        solidityFileBase = options._[0];
+      var solidityFileBase;
+      let solidityFile;
+      let buildJsonPath;
+
+      try {
+        if (options._.length === 0) {
+          solidityFileBase = path.basename(buildJson, '.json');
+        } else {
+          solidityFileBase = options._[0];
+        }
+
+        if (! solidityFileBase.endsWith('.sol')) {
+          solidityFileBase += '.sol';
+        }
+
+        solidityFile = path.join(contractsDir, solidityFileBase);
+        if (options.debug) {
+          options.logger.log(`Solidity file used: ${solidityFile}`);
+        }
+        buildJsonPath = path.join(buildDir, buildJson);
+
+      } catch (err) {
+        done(err);
       }
 
-      if (! solidityFileBase.endsWith('.sol')) {
-        solidityFileBase += '.sol';
-      }
+      // console.log(`Reading ${buildJsonPath}`);
 
-      solidityFile = path.join(contractsDir, solidityFileBase);
-      if (options.debug) {
-        console.log(`Solidity file used: ${solidityFile}`);
-      }
-      buildJsonPath = path.join(buildDir, buildJson);
+      let client = new armlet.Client(
+        {
+          // NOTE: authentication is changing in the next API release
+          apiKey: process.env.MYTHRIL_API_KEY,
+          userEmail: process.env.MYTHRIL_API_KEY || 'bogus@example.com'
+        });
 
-    } catch (err) {
-      done(err);
+      const buildObj = JSON.parse(fs.readFileSync(buildJsonPath, 'utf8'));
+
+      // console.log(JSON.stringify(buildObj, null, 4));
+      options.data = mythril.truffle2MythrilJSON(buildObj);
+      options.data.analysisMode = options.analysisMode || 'full';
+
+      client.analyze(options)
+        .then(issues => {
+          const formatter = getFormatter(options.style);
+          let esIssues = mythril.issues2Eslint(issues, buildObj, options);
+          // console.log(esIssues); // debug
+          esReporter.printReport(esIssues, solidityFile, formatter, options.logger.log);
+          done(null, [], []);
+        }).catch(err => {
+          done(err);
+        });
     }
 
-    // console.log(`Reading ${buildJsonPath}`);
+    const Contracts = require("truffle-workflow-compile");
 
-    let client = new armlet.Client(
-      {
-        // NOTE: authentication is changing in the next API release
-        apiKey: process.env.MYTHRIL_API_KEY,
-        userEmail: process.env.MYTHRIL_API_KEY || 'bogus@example.com'
-      });
-
-    const buildObj = JSON.parse(fs.readFileSync(buildJsonPath, 'utf8'));
-
-    // console.log(JSON.stringify(buildObj, null, 4));
-    options.data = mythril.truffle2MythrilJSON(buildObj);
-    options.data.analysisMode = options.analysisMode || 'full';
-
-    client.analyze(options)
-      .then(issues => {
-        const formatter = getFormatter(options.style);
-        let esIssues = mythril.issues2Eslint(issues, buildObj, options);
-        // console.log(esIssues); // debug
-        esReporter.printReport(esIssues, solidityFile, formatter, console.log);
-        done(null, [], []);
-      }).catch(err => {
-        done(err);
-      });
+    Contracts.compile(config,
+                      function(arg) {
+                        if (arg !== null) {
+                          options.logger.log(`compile returns ${arg}`);
+                        }
+                    analyzeWithBuildDir()
+                  });
   }
 }
 
