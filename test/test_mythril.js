b32672c33e0a754ddcddc405e1aac4faf5b8e6d7
Daniyar Chambylov <dan.chambylov@gmail.com>
2018-12-19 23:20:48 +0700

    add tests for mythril.js
    * issues2Eslint
    * Info class instantiating

--- test/test_mythril.js
+++ test/test_mythril.js
@@ -30,24 +30,80 @@ describe('mythril', function() {
             assert.equal(mess, '\'Test\' \'string\'.');
         });
     });
+
     describe('truffle2MythrilJSON', () => {
-        let trufflJSON;
+        let truffleJSON;
         const contractJSON = `${__dirname}/sample-truffle/simple_dao/build/contracts/SimpleDAO.json`;
         
         beforeEach(done => { 
             fs.readFile(contractJSON, 'utf8', (err, data) => {
                 if (err) return done(err);
-                trufflJSON = JSON.parse(data);
+                truffleJSON = JSON.parse(data);
                 done();
             })
         });
 
         it('should not mutate truffleJSON', (done) => {
-            const newJSON = mythrilRewired.truffle2MythrilJSON(trufflJSON);
-            assert.notEqual(trufflJSON, newJSON);
-            assert.equal(newJSON.sourceList[0], trufflJSON.ast.absolutePath);
-            assert.equal(newJSON.sources[trufflJSON.contractName], trufflJSON.source);
+            const newJSON = mythrilRewired.truffle2MythrilJSON(truffleJSON);
+            assert.notEqual(truffleJSON, newJSON);
+            assert.deepEqual(newJSON.sourceList, [truffleJSON.ast.absolutePath]);
+            assert.equal(newJSON.sources[truffleJSON.contractName], truffleJSON.source);
             done();
         });
-    })
+    });
+
+    describe('issues2Eslint', () => {
+        let truffleJSON;
+        const contractJSON = `${__dirname}/sample-truffle/simple_dao/build/contracts/SimpleDAO.json`;
+        
+        beforeEach(done => { 
+            fs.readFile(contractJSON, 'utf8', (err, data) => {
+                if (err) return done(err);
+                truffleJSON = JSON.parse(data);
+                done();
+            })
+        });
+
+        it('calling issues2Eslint with truffleJSON should not break', (done) => {
+            const res = mythrilRewired.issues2Eslint([], truffleJSON, {});
+            assert.deepEqual(res, []);
+            done();
+        });
+    });
+
+    describe('Info class', () => {
+        let truffleJSON;
+        const InfoClass = mythrilRewired.__get__('Info');
+        const contractJSON = `${__dirname}/sample-truffle/simple_dao/build/contracts/SimpleDAO.json`;
+        
+        beforeEach(done => { 
+            fs.readFile(contractJSON, 'utf8', (err, data) => {
+                if (err) return done(err);
+                truffleJSON = JSON.parse(data);
+                done();
+            })
+        });
+
+        it('should instantiate correctly', (done) => {
+            const info = new InfoClass([], truffleJSON);
+            assert.deepEqual(info.issues, []);
+            assert.deepEqual(info.buildObj, truffleJSON);
+            assert.deepEqual(info.ast, truffleJSON.ast);
+            assert.deepEqual(info.sourceMap, truffleJSON.sourceMap);
+            assert.deepEqual(info.deployedSourceMap, truffleJSON.deployedSourceMap);
+            done();
+        });
+    
+        it('should instantiate correctly after truffle2MythrilJSON was called', (done) => {
+            mythrilRewired.truffle2MythrilJSON(truffleJSON);
+    
+            const info = new InfoClass([], truffleJSON);
+            assert.deepEqual(info.issues, []);
+            assert.deepEqual(info.buildObj, truffleJSON);
+            assert.deepEqual(info.ast, truffleJSON.ast);
+            assert.deepEqual(info.sourceMap, truffleJSON.sourceMap);
+            assert.deepEqual(info.deployedSourceMap, truffleJSON.deployedSourceMap);
+            done();
+        });
+    });
 });
