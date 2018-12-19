cc5dcc47700dc505196b358b5b155f3033df36b1
Daniyar Chambylov <dan.chambylov@gmail.com>
2018-12-19 19:07:18 +0700

    Add test for truffle2MythrilJSON

--- test/test_mythril.js
+++ test/test_mythril.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const rewire = require('rewire');
+const fs = require('fs');
 
 const mythrilRewired = rewire('../lib/mythril');
 
@@ -29,4 +30,24 @@ describe('mythril', function() {
             assert.equal(mess, '\'Test\' \'string\'.');
         });
     });
+    describe('truffle2MythrilJSON', () => {
+        let trufflJSON;
+        const contractJSON = `${__dirname}/sample-truffle/simple_dao/build/contracts/SimpleDAO.json`;
+        
+        beforeEach(done => { 
+            fs.readFile(contractJSON, 'utf8', (err, data) => {
+                if (err) return done(err);
+                trufflJSON = JSON.parse(data);
+                done();
+            })
+        });
+
+        it('should not mutate truffleJSON', (done) => {
+            const newJSON = mythrilRewired.truffle2MythrilJSON(trufflJSON);
+            assert.notEqual(trufflJSON, newJSON);
+            assert.equal(newJSON.sourceList[0], trufflJSON.ast.absolutePath);
+            assert.equal(newJSON.sources[trufflJSON.contractName], trufflJSON.source);
+            done();
+        });
+    })
 });
