c8f158a95e570718c70f642ceddb1351cb6190fb
Daniyar Chambylov <dan.chambylov@gmail.com>
2018-12-21 08:24:04 +0700

    Add tests for helpers.js
    * printVersion
    * getContractsDetails

---
+++ test/test_helpers.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const armlet = require('armlet');
+const sinon = require('sinon');
+const contracts = require("truffle-workflow-compile");
+const helpers = require('../helpers');
+const trufstuf = require('../lib/trufstuf');
+
+
+describe('helpers.js', function() {
+  it('should call printVersion', async () => {
+    const stubAPI = sinon.stub(armlet, 'ApiVersion').returns('1.0.0');
+    const stubLog = sinon.stub(console, 'log');
+    const version = await helpers.printVersion();
+    assert.equal(version, '1.0.0')
+    assert.ok(stubAPI.called);
+    assert.ok(stubLog.called);
+    stubLog.restore();
+  });
+
+  it('should get contract json and sol locations', async () => {
+    sinon
+      .stub(trufstuf, 'getContractsDir')
+      .returns('/tests/contracts');
+    sinon
+      .stub(trufstuf, 'guessTruffleBuildJson')
+      .returns('/tests/build/contracts');
+
+    const details = await helpers.getSolidityDetails({
+      _: ['/tests.json'],
+      working_drectory: '/tests',
+      contracts_build_directory: '/tests/build',
+    });
+    assert.equal(details.solidityFile, '/tests/contracts/Contract.sol')
+    assert.equal(details.buildJsonPath, '/tests/build/contracts/Contract.json')
+  });
+});
