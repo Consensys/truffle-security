63c27bfe977cf202f61602bee6f8c2d38be64bfe
Daniyar Chambylov <dan.chambylov@gmail.com>
2019-01-15 16:11:53 +0700

    Add tests for mythXresults2Eslint function

--- test/test_issue2eslint.js
+++ test/test_issue2eslint.js
@@ -169,5 +169,66 @@ describe('issues2Eslint', function() {
             spyIsVariableDeclaration.restore();
             spyIsDynamicArray.restore();
         });
+
+        it('should call mythXresults2Eslint correctly', () => {
+            const issues = [{
+                address: 444,
+                contract: 'TestContract',
+                description: 'Issue description',
+                function: '_function_0x00000000',
+                title: 'Issie',
+                type: 'Warning',
+                'swc-id': 'xxx',
+                tool: 'mythril'
+            }];
+
+            const spyIssue2EsLintNew = sinon.spy(InfoClass.prototype, 'issue2EsLintNew');
+            const stubIsIgnorable = sinon.stub(InfoClass.prototype, 'isIgnorable');
+            const loggerStub = sinon.stub();
+            stubIsIgnorable.returns(false);
+            const mythXresults2Eslint = rewired.__get__('mythXresults2Eslint');
+            const res = mythXresults2Eslint([{
+                issues,
+                sourceFormat: 'evm-byzantium-bytecode',
+                sourceType: 'raw-bytecode',
+                sourceList: ['"0x608060405260043610610061576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168062362a95146100665780632e1a7d4d1461009c57806359f1286d146100c9578063d5d44d8014610120575b600080fd5b61009a600480360381019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190505050610177565b005b3480156100a857600080fd5b506100c7600480360381019080803590602001909291905050506101c6565b005b3480156100d557600080fd5b5061010a600480360381019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190505050610294565b6040518082815260200191505060405180910390f35b34801561012c57600080fd5b50610161600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102dc565b6040518082815260200191505060405180910390f35b346000808373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000206000828254019250508190555050565b806000803373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002054101515610291573373ffffffffffffffffffffffffffffffffffffffff168160405160006040518083038185875af192505050151561024457600080fd5b806000803373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600082825403925050819055505b50565b60008060008373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020549050919050565b600060205280600052604060002060009150905054815600a165627a7a72305820995dd360cfe1e03c0dded401ac885f902c03677f72bdcce6d8d845db1f313dca0029"'],
+            }], truffleJSON, { logger: { log: loggerStub }})
+            assert.equal(res.length, 1);
+            assert.ok(spyIssue2EsLintNew.called);
+            assert.ok(stubIsIgnorable.called);
+            spyIssue2EsLintNew.restore();
+            stubIsIgnorable.restore();
+        });
+
+        it('should call mythXresults2Eslint and return empty array when issue is ignored', () => {
+            const issues = [{
+                address: 444,
+                contract: 'TestContract',
+                description: 'Issue description',
+                function: '_function_0x00000000',
+                title: 'Issie',
+                type: 'Warning',
+                'swc-id': 'xxx',
+                tool: 'mythril'
+            }];
+
+            const spyIssue2EsLintNew = sinon.spy(InfoClass.prototype, 'issue2EsLintNew');
+            const stubIsIgnorable = sinon.stub(InfoClass.prototype, 'isIgnorable');
+            const loggerStub = sinon.stub();
+            stubIsIgnorable.returns(true);
+            const mythXresults2Eslint = rewired.__get__('mythXresults2Eslint');
+            const res = mythXresults2Eslint([{
+                issues,
+                sourceFormat: 'evm-byzantium-bytecode',
+                sourceType: 'raw-bytecode',
+                sourceList: ['"0x608060405260043610610061576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168062362a95146100665780632e1a7d4d1461009c57806359f1286d146100c9578063d5d44d8014610120575b600080fd5b61009a600480360381019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190505050610177565b005b3480156100a857600080fd5b506100c7600480360381019080803590602001909291905050506101c6565b005b3480156100d557600080fd5b5061010a600480360381019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190505050610294565b6040518082815260200191505060405180910390f35b34801561012c57600080fd5b50610161600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102dc565b6040518082815260200191505060405180910390f35b346000808373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000206000828254019250508190555050565b806000803373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002054101515610291573373ffffffffffffffffffffffffffffffffffffffff168160405160006040518083038185875af192505050151561024457600080fd5b806000803373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600082825403925050819055505b50565b60008060008373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020549050919050565b600060205280600052604060002060009150905054815600a165627a7a72305820995dd360cfe1e03c0dded401ac885f902c03677f72bdcce6d8d845db1f313dca0029"'],
+            }], truffleJSON, { logger: { log: loggerStub }})
+            console.log('>>>>>>>>>>>>>>>>>>>', res);
+            assert.equal(res.length, 0);
+            assert.ok(spyIssue2EsLintNew.notCalled);
+            assert.ok(stubIsIgnorable.called);
+            spyIssue2EsLintNew.restore();
+            stubIsIgnorable.restore();
+        });
     });
 });
